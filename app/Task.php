56a8254fcb30a5ce5a4cbd6052e168b76fdd5921
Jason Majors <jasonrmajors@gmail.com>
2016-02-13 11:30:04 -0800

    Tasks table, Task model, and basic auth up and running

---
+++ app/Task.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Task extends Model
+{
+    protected $fillable = ['name'];
+
+    /**
+    * Get the user that owns the task.
+    */
+    public function user()
+    {
+    	return $this0>belongsTo(User::class);
+    }
+}
